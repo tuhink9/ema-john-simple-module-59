b7ecd2b337a771b36275a5c8538c1b335f38f121
putk9 <tuhinkabir523@gmail.com>
2022-04-11 00:05:42 +0600

    form added

--- src/components/SignUp/SignUp.js
+++ src/components/SignUp/SignUp.js
@@ -1,25 +1,29 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import './Login.css'
+import './SignUp.css'
 
 const Login = () => {
     return (
         <div className='form-container'>
             <div>
-                <h1 className='form-title'>Login</h1>
+                <h1 className='form-title'>Sign Up</h1>
                 <form action="">
                 <div className="input-group">
                     <label htmlFor='email'>Email</label>
-                    <input type="email" name="email" id="" />
+                    <input type="email" name="email" id="" required/>
                 </div>
                 <div className="input-group">
                     <label htmlFor='password'>Password</label>
                     <input type="password" name="password" id="" required/>
                 </div>
-                <input className='form-submit' type="submit" value="Login" required/>
+                <div className="input-group">
+                    <label htmlFor='confirm-password'>Confirm Password</label>
+                    <input type="password" name="confirm-password" id="" required/>
+                </div>
+                <input className='form-submit' type="submit" value="SignUp" />
                 </form>
                 <p>
-                    New to Ema-John? <Link className='form-link' to='/signup'>Create Account</Link>
+                    Already Have An Account? <Link className='form-link' to='/login'>Login</Link>
                 </p>
             </div>
         </div>
